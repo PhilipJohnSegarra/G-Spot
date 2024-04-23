980b16f4c03e74a4efa5bb7d4cffdc8a4d172413
philip <philipjohn.segarra@gmail.com>
2024-04-23 18:10:19 +0800

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Text.Format;
+using AndroidX.AppCompat.App;
+
+namespace G_Spot
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Intent intent = new Intent(this, typeof(SignInActivity));
+
+            StartActivity(intent);
+            
+        }
+    }
+}
---
+++ SignInActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace G_Spot
+{
+    [Activity(Label = "SignInActivity")]
+    public class SignInActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.SignIn);
+
+            // Create your application here
+        }
+    }
+}
